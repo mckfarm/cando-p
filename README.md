2b1175eb6ad90787d423908ac15963bee54eda34
mckfarm <51832180+mckfarm@users.noreply.github.com>
2021-05-28 14:53:27 -0500

    probe parse, readme
    wrote probe parse script for Hach control boxes, explained in readme

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # CANDO+P
 ## Scripts for parsing, analysis, and visualization of CANDO+P bioreactor
 
-
-hachprobeparse.py - parses Hach probe logs and saves them as csv
+hachprobeparse.py - parses Hach probe logs and saves them as csv files
+- hach_dl_date.csv: outputs of sc1000 and sc200 sensors matched by time
+- sc200_dl_date.csv: daily log outputs of sc200 sensors
+- sc1000_dl_date.csv: daily log outputs of sc1000 sensors
+- sc200_el_date.csv: event log of sc200
+- sc1000_el_date.csv: event log of sc1000
--- hachprobeparse.py
+++ hachprobeparse.py
@@ -4,3 +4,246 @@ Purpose: parsing through probe log files
 Output: summary file of probe outputs
 Date: 5/21/21
 '''
+
+def hach_sc1000(directory, out_file, skip_range, header):
+    '''
+    sc1000 data processing
+    inputs:
+        directory - where sc1000 outlogs are saved AND with file prefix
+            Daily log vs event log
+        out_file - output directory and file name
+        skip_range - raw csv skip lines, 21 for daily log, 18 for event log
+        header - list for header row
+            ["date","do_ppm","temp_c"] for daily log
+            ["date","event","info1","info2"] for event log
+    '''
+    import glob
+    import csv
+
+    alldata_list = [] # empty list to save results to
+
+    filenames = glob.glob(directory)
+
+    for name in filenames:
+        with open(name) as f:
+            reader = csv.reader(f)
+            for skip in range(skip_range):
+                next(reader)
+            data_list = list(reader)
+        for entry in data_list:
+            alldata_list.append(entry)
+            
+    
+    with open(out_file, "w", newline="") as write_file:
+        writer = csv.writer(write_file)
+        writer.writerow(header)
+        writer.writerows(alldata_list)
+    
+
+def hach_sc200_orp(directory):
+    '''
+    sc200 data processing
+    inputs:
+        directory - input directory with *.xml specified
+    outputs: 
+        list of lists with orp data
+    '''
+    import xml.etree.ElementTree as ET
+    import glob
+    
+    filenames = glob.glob(directory)
+    
+    alldata_list = []
+    
+    for name in filenames:
+        with open(name) as f:
+            tree=ET.parse(f)
+        
+        root = tree.getroot()
+    
+        # time
+        time = []
+        for i in root.findall("Entry"):
+            value = i.find("Time")
+            time.append(value.text)
+        time.pop(0) # have to remove first row because it comes up as column name
+        time = [x.lstrip().rstrip() for x in time]
+            
+        # orp
+        orp = []
+        for child in root.findall(".//CH1"):
+            value = child.find(".//Data")
+            orp.append(value.text)
+        orp.pop(0)
+        orp = [x.lstrip().rstrip() for x in orp]
+    
+        rows = list(zip(time,orp)) # turning columns into rows for csv writing
+        
+        alldata_list.extend(rows)
+        
+    return alldata_list
+     
+
+def hach_sc200_ph(directory):
+    '''
+    sc200 data processing
+    inputs:
+        directory - input directory with *.xml specified
+    outputs: 
+        list of lists with ph data
+    '''
+    import xml.etree.ElementTree as ET
+    import glob
+    
+    filenames = glob.glob(directory)
+    
+    alldata_list = []
+    
+    for name in filenames:
+        with open(name) as f:
+            tree=ET.parse(f)
+            
+        root = tree.getroot()
+    
+        # time
+        time = []
+        for i in root.findall("Entry"):
+            value = i.find("Time")
+            time.append(value.text)
+        time.pop(0) # have to remove first row because it comes up as column name
+        time = [x.lstrip().rstrip() for x in time]
+            
+        # ph
+        ph = []
+        for child in root.findall(".//CH1"):
+            value = child.find(".//Data")
+            ph.append(value.text)
+        ph.pop(0)
+        ph = [x.lstrip().rstrip() for x in ph]
+            
+        temp = []
+        for child in root.findall(".//CH2"):
+            value = child.find(".//Data")
+            temp.append(value.text)    
+        temp.pop(0)
+        temp = [x.lstrip().rstrip() for x in temp]
+        
+        rows = list(zip(time,ph,temp)) # turning columns into rows for csv writing
+         
+        alldata_list.extend(rows)
+
+    return alldata_list   
+
+
+def hach_sc200_dl(ph, orp, out_file):
+    '''
+    sc200 data processing
+    inputs:
+        directory - ph and orp list of lists from functions
+        out_file - output directory and file name
+    '''  
+    import pandas as pd
+    
+    df_ph = pd.DataFrame(ph,columns=["date_time","ph","temp"])
+    df_orp = pd.DataFrame(orp,columns=["date_time","orp"])
+    
+    df = pd.merge(df_orp,df_ph)
+    
+    df.to_csv(out_file,index=False)
+
+    
+def hach_sc200_el(directory, out_file):
+    '''
+    sc200 outlog - same base script as sc1000, skip and header built-in since its set
+    inputs:
+        directory - where sc200 outlog csvs are saved
+        out_file - output directory and file name
+    '''
+    import glob
+    import csv
+
+    alldata_list = [] # empty list to save results to
+
+    filenames = glob.glob(directory)
+
+    for name in filenames:
+        with open(name) as f:
+            reader = csv.reader(f)
+            for skip in range(2):
+                next(reader)
+            data_list = list(reader)
+        for entry in data_list:
+            alldata_list.append(entry)
+    
+    header = ["date_time","event","event_code","info1","info2","info3","info4"]
+    with open(out_file, "w", newline="") as write_file:
+        writer = csv.writer(write_file)
+        writer.writerow(header)
+        writer.writerows(alldata_list)
+        
+def hach_dl(sc200,sc1000,out_file):
+    '''
+    sc200 data processing
+    inputs:
+        sc200 - sc200 dl filename
+        sc1000 - sc1000 dl filename
+        out_file - output directory and name
+    '''  
+    import pandas as pd
+    
+    df200 = pd.read_csv(sc200,usecols=[0,1,2,3])
+    df1000 = pd.read_csv(sc1000,usecols=[0,1,2])
+    
+    df200["date_time"] = df200["date_time"].astype(str)
+    df1000["date_time"] = df1000["date_time"].astype(str)
+    
+    df = pd.merge(df1000,df200,on="date_time",how="left")
+    df.to_csv(out_file,index=False)
+
+# 5/28/21 data logs
+out_sc1000_dl = "sc1000_dl_21.5.28.csv"
+out_sc200_dl = "sc200_dl_21.5.28.csv"
+
+out_sc1000_el = "sc1000_el_21.5.28.csv"
+out_sc200_el = "sc200_el_21.5.28.csv"
+
+out_hach = "hach_dl_21.5.28.csv"
+
+dir_sc1000_dl = r"C:\Users\mckyf\Box\CANDO+P and N2O\CANDO+P Reactor 2021\Operation and Logs\Raw reactor logs\21.28.5\sc1000\MID00000_IID00042_SN142700000011\LDO2_142700000011_0_42_DL_210428_210528.csv"
+dir_sc1000_el = r"C:\Users\mckyf\Box\CANDO+P and N2O\CANDO+P Reactor 2021\Operation and Logs\Raw reactor logs\21.28.5\sc1000\MID00000_IID00042_SN142700000011\LDO2_142700000011_0_42_EL_210428_210528.csv"
+
+dir_sc200_dl_orp = r"C:\Users\mckyf\Box\CANDO+P and N2O\CANDO+P Reactor 2021\Operation and Logs\Raw reactor logs\21.28.5\sc200\PH_ORP_0_34_ORP COMBO\PH_ORP_1406C1046936_0_34_DL_ 210401_ 000000.xml"
+dir_sc200_dl_ph = r"C:\Users\mckyf\Box\CANDO+P and N2O\CANDO+P Reactor 2021\Operation and Logs\Raw reactor logs\21.28.5\sc200\PH_ORP_0_34_pH COMBO\PH_ORP_1406C1046183_0_34_DL_ 210401_ 000000.xml"
+dir_sc200_el = r"C:\Users\mckyf\Box\CANDO+P and N2O\CANDO+P Reactor 2021\Operation and Logs\Raw reactor logs\21.28.5\sc200\SC200_0_39_SC200_0000000000\PH_ORP_1406C1046183_0_34_EL_ 210401_ 000000.csv"
+
+hach_sc1000(dir_sc1000_dl, out_sc1000_dl, 21, ["date_time","do_ppm","temp_c"])
+orp = hach_sc200_orp(dir_sc200_dl_orp)
+ph = hach_sc200_ph(dir_sc200_dl_ph)
+hach_sc200_dl(ph,orp,out_sc200_dl)
+
+hach_sc1000(dir_sc1000_el, out_sc1000_el, 18, ["date_time","event","info1","info2"])
+hach_sc200_el(dir_sc200_el,out_sc200_el)
+
+hach_dl(out_sc200_dl,out_sc1000_dl, out_hach)
+
+# 5/21/21 data logs
+# out_sc1000_dl = "sc1000_dl_21.5.21.csv"
+# out_sc200_dl = "sc200_dl_21.5.21.csv"
+
+# dir_sc1000_dl = r"C:\Users\mckyf\Box\CANDO+P and N2O\CANDO+P Reactor 2021\Operation and Logs\Raw reactor logs\21.21.5\sc1000\dailylog\000002062676\MID00000_IID00042_SN142700000011\LDO2_142700000011_0_42_DL_*"
+# dir_sc1000_el = r"C:\Users\mckyf\Box\CANDO+P and N2O\CANDO+P Reactor 2021\Operation and Logs\Raw reactor logs\21.21.5\sc1000\dailylog\000002062676\MID00000_IID00042_SN142700000011\LDO2_142700000011_0_42_EL_*"
+
+# dir_sc200_dl_orp = r"C:\Users\mckyf\Box\CANDO+P and N2O\CANDO+P Reactor 2021\Operation and Logs\Raw reactor logs\21.21.5\sc200\PH_ORP_0_34_ORP COMBO\*.xml"
+# dir_sc200_dl_ph = r"C:\Users\mckyf\Box\CANDO+P and N2O\CANDO+P Reactor 2021\Operation and Logs\Raw reactor logs\21.21.5\sc200\PH_ORP_0_34_pH COMBO\*.xml"
+# dir_sc200_el = r"C:\Users\mckyf\Box\CANDO+P and N2O\CANDO+P Reactor 2021\Operation and Logs\Raw reactor logs\21.21.5\sc200\SC200_0_39_SC200_0000000000\*csv"
+
+# hach_sc1000(dir_sc1000_dl, out_sc1000_dl, 21, ["date_time","do_ppm","temp_c"])
+# orp = hach_sc200_orp(dir_sc200_dl_orp)
+# ph = hach_sc200_ph(dir_sc200_dl_ph)
+# hach_sc200_dl(ph,orp,out_sc200_dl)
+
+# hach_sc1000(dir_sc1000_el, "sc1000_el_21.5.21.csv", 18, ["date_time","event","info1","info2"])
+# hach_sc200_el(dir_sc200_el,"sc200_el_51.5.21.csv")
+
+# hach_dl(out_sc200_dl,out_sc1000_dl,"hach_dl_21.5.21.csv")
+
